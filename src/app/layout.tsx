e7d4567d200d0641b94e53c2945ed457c0aa3e5f
Aaron Mills <millsa2020@fau.edu>
2025-05-30 09:49:21 -0400

    Update styling: Add mint-colored borders to sections, improve dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Script from 'next/script'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 config.autoAddCss = false
 
@@ -31,11 +31,16 @@ export default function RootLayout({
         <Script 
           src="https://kit.fontawesome.com/3c09bcb51a.js" 
           crossOrigin="anonymous"
+          strategy="afterInteractive"
         />
         <Script 
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+          strategy="afterInteractive"
         />
-        <Script src="/js/scripts.js" />
+        <Script 
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   )
