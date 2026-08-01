45f925b1baa83c4d07e87d905ebcced96827adbb
Aaron Mills <millsa2020@fau.edu>
2026-08-01 01:19:39 -0400

    Add Smash Spirit Clash to sample projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -463,6 +463,7 @@ export default function Home() {
                 title="Sample Projects"
                 description="Mock up, sample projects, and scrapped ideas."
                 links={[
+                  { href: "https://smash-spirit-clash.pages.dev", text: "Smash Spirit Clash" },
                   { href: "https://royalchat.aaronmills.co", text: "Royal Chat" },
                   { href: "https://voicenoter.aaronmills.co", text: "Voice Noter" },
                   { href: "https://createinc.aaronmills.co", text: "Create.Inc" },
